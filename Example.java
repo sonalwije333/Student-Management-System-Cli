748def3862d183f78a258b0c0e3c2cdc64b1769a
Sonal Wijenayaka <sonalroxtar@gmail.com>
2024-11-08 22:13:27 +0530

    files added

---
+++ Example.java
@@ -0,0 +1,134 @@
+import java.util.*;
+
+class Marks {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("---------------------------------------------------------------------------------");
+            System.out.println("|\t\t WELCOME TO STUDENTS MARKS MANAGEMENT SYSTEM \t\t\t|");
+            System.out.println("---------------------------------------------------------------------------------");
+            System.out.println("[1] Add New Student\t\t\t[2] Add New Student With Marks");
+            System.out.println("[3] Add Marks\t\t\t\t[4] Update Student Details");
+            System.out.println("[5] Update Marks\t\t\t[6] Delete Students");
+            System.out.println("[7] Print Student Details\t\t[8] Print Students Ranks");
+            System.out.println("[9] Best in Programming Fundamental\t[10] Best In Database Management System");
+            System.out.println("[0] Exit");
+            System.out.print("\nEnter an option to continue > ");
+            
+            int option = input.nextInt();
+            
+            switch(option) {
+                case 1:
+                    addNewStudent();
+                    break;
+                case 2:
+                    addNewStudentWithMarks();
+                    break;
+                case 3:
+                    addMarks();
+                    break;
+                case 4:
+                    updateStudentDetails();
+                    break;
+                case 5:
+                    updateMarks();
+                    break;
+                case 6:
+                    deleteStudents();
+                    break;
+                case 7:
+                    printStudentDetails();
+                    break;
+                case 8:
+                    printStudentsRanks();
+                    break;
+                case 9:
+                    bestInProgrammingFundamentals();
+                    break;
+                case 10:
+                    bestInDatabaseManagement();
+                    break;
+                case 0:
+                    System.out.println("Exiting the system. Goodbye!");
+                    input.close();
+                    return;  
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+    }
+
+    public static void addNewStudent() {
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("|\t\t\t\tAdd New Student \t\t\t\t|");
+        System.out.println("---------------------------------------------------------------------------------");
+
+
+		
+	}
+
+    public static void addNewStudentWithMarks() {
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("|\t\t\t\tAdd New Student With Marks \t\t\t|");
+        System.out.println("---------------------------------------------------------------------------------");
+      
+    }
+
+    public static void addMarks() {
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("|\t\t\t\tAdd Marks   \t\t\t\t\t|");
+        System.out.println("---------------------------------------------------------------------------------");
+        
+    }
+
+    public static void updateStudentDetails() {
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("|\t\t\t\tUpdate Student Details \t\t\t\t|");
+        System.out.println("---------------------------------------------------------------------------------");
+       
+    }
+
+    public static void updateMarks() {
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("|\t\t\t\tUpdate Marks  \t\t\t\t\t|");
+        System.out.println("---------------------------------------------------------------------------------");
+        
+    }
+
+    public static void deleteStudents() {
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("|\t\t\t\t Delete Students \t\t\t\t\t|");
+        System.out.println("---------------------------------------------------------------------------------");
+        
+    }
+
+    public static void printStudentDetails() {
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("|\t\t\t\t Print Student Details \t\t\t\t|");
+        System.out.println("---------------------------------------------------------------------------------");
+        
+    }
+
+    public static void printStudentsRanks() {
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("|\t\t\t\tPrint Students Ranks  \t\t\t\t|");
+        System.out.println("---------------------------------------------------------------------------------");
+      
+    }
+
+    public static void bestInProgrammingFundamentals() {
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("|\t\t\t  Best in Programming Fundamentals     \t\t\t|");
+        System.out.println("---------------------------------------------------------------------------------");
+       
+    }
+
+    public static void bestInDatabaseManagement() {
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("|\t\t       Best In Database Management System   \t\t\t|");
+        System.out.println("---------------------------------------------------------------------------------");
+       
+    }
+}
